eab02d1e7703ad85b7e553a9ca6585405940fb4c
shadi95 <s0182815896@gmail.com>
2021-03-30 20:39:03 +0800

    product table

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -12,4 +12,8 @@ class Product extends Model
     protected $fillable = [
         'title', 'description', 'unit', 'price', 'total', 
     ];
+
+    public function images(){
+        return $this->hasMany(Image::class);
+    }
 }
