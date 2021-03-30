de4f19bc974ef141df47c22e573c483d6035f826
shadi95 <s0182815896@gmail.com>
2021-03-30 20:36:58 +0800

    image table

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -12,4 +12,8 @@ class Image extends Model
     protected $fillable = [
         'url', 'product_id', 
     ];
+
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
