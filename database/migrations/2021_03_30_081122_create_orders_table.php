8820b2509bd7fc824f1e56cc8c82f36f252ff103
shadi95 <s0182815896@gmail.com>
2021-03-30 16:16:57 +0800

    ordera table schema

--- database/migrations/2021_03_30_081122_create_orders_table.php
+++ database/migrations/2021_03_30_081122_create_orders_table.php
@@ -15,6 +15,10 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('user_id');
+            $table->bigInteger('payment_id');
+            $table->bigInteger('cart_id'); 
+            $table->dateTime('oder_date');
             $table->timestamps();
         });
     }
