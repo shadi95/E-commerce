bf5865fa71d40465b48b046bee36920635d830e5
shadi95 <s0182815896@gmail.com>
2021-03-30 16:23:03 +0800

    carts table schema

--- database/migrations/2021_03_30_081729_create_carts_table.php
+++ database/migrations/2021_03_30_081729_create_carts_table.php
@@ -15,6 +15,8 @@ class CreateCartsTable extends Migration
     {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
+            $table->text('cart_item');
+            $table->double('total');
             $table->timestamps();
         });
     }
