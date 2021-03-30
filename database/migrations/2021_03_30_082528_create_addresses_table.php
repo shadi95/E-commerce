1e1fb51d9110d1890d1065a6caf2befea9b8c98f
shadi95 <s0182815896@gmail.com>
2021-03-30 16:31:24 +0800

    addresses table schema

--- database/migrations/2021_03_30_082528_create_addresses_table.php
+++ database/migrations/2021_03_30_082528_create_addresses_table.php
@@ -15,6 +15,12 @@ class CreateAddressesTable extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
+            $table->string('street_name');
+            $table->string('street_number');
+            $table->string('city');
+            $table->string('state');
+            $table->string('country');
+            $table->string('post_code');
             $table->timestamps();
         });
     }
